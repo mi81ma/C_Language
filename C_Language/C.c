ea1c172b7a03300dca8e0e999e04525e8c2f6c0e
masato <masato@m-mbp.local>
2018-10-05 09:57:08 +0800

    input Text and Number

--- C_Language/C.c
+++ C_Language/C.c
@@ -3,6 +3,24 @@
 #include <stdio.h>
 
 void main(){
-    printf("ABC\n");
-    printf("DEF\n");
+    // "This is test line" into Array "a[]"
+    static char a[25] = "This is test line.";
+
+    // declare int b
+    int b;
+
+    // 300 as hexadecimal number into b
+    b = 0x300;
+
+    // %s is to represent a[] as String
+    printf(" **** Strings is : %s ****\n", a);
+
+    // %x is to represent b as hexadecimal number
+    printf(" **** Value   is : %x ****\n", b);
+
+
+    // Output the following:
+    //  **** Strings is : This is test line. ****
+    //  **** Value   is : 300 ****
+
 }
